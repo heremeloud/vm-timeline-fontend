8200afc089e8ba4efddc9a9ee61a58a72ff7994d
Fran <hopeli919@gmail.com>
2025-12-04 20:14:51 -0800

    debug

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL
+    baseURL: import.meta.env.VITE_API_URL,
 });
+
+console.log("API URL Loaded:", import.meta.env.VITE_API_URL);
 
 // Add token to all requests
 api.interceptors.request.use((config) => {
