0f7f76f0d3c6b285a7b63ebade564adb89b5ab14
Fran <hopeli919@gmail.com>
2025-12-04 21:08:59 -0800

    fixed bug

--- src/components/TweetEmbed.jsx
+++ src/components/TweetEmbed.jsx
@@ -1,26 +1,16 @@
 import { useEffect } from "react";
 
 export default function TweetEmbed({ url }) {
-    // useEffect(() => {
-    //     if (!window._twScriptLoaded) {
-    //         const tw = document.createElement("script");
-    //         tw.src = "https://platform.twitter.com/widgets.js";
-    //         tw.async = true;
-    //         document.body.appendChild(tw);
-    //         window._twScriptLoaded = true;
-    //     }
-    // }, []);
     useEffect(() => {
-        if (!window._twitterLoaded) {
+        if (!window._twScriptLoaded) {
             const tw = document.createElement("script");
             tw.src = "https://platform.twitter.com/widgets.js";
             tw.async = true;
             document.body.appendChild(tw);
-            window._twitterLoaded = true;
-        } else {
-            window.twttr?.widgets?.load();
+            window._twScriptLoaded = true;
         }
-    }, [posts]);
+    }, []);
+    
 
     return (
         <div className="tweet-embed-wrapper">
