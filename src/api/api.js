b8117b96325e7a73c9070d412897b1cb25970723
Fran <hopeli919@gmail.com>
2026-06-15 14:39:18 -0700

    update

--- src/api/api.js
+++ src/api/api.js
@@ -1,15 +1,15 @@
 import axios from "axios";
 import { setupCache } from "axios-cache-interceptor";
 
-// const api = setupCache(axios.create({
-//     baseURL: import.meta.env.VITE_API_URL ,
-// }), {
-//     ttl: 1000 * 60 * 10,  // cache 10 minutes
-// });
+const api = setupCache(axios.create({
+    baseURL: import.meta.env.VITE_API_URL ,
+}), {
+    ttl: 1000 * 60 * 10,  // cache 10 minutes
+});
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || "http://localhost:8000",
-});
+//     baseURL: import.meta.env.VITE_API_URL || "http://localhost:8000",
+// });
 
 // Add token to all requests
 api.interceptors.request.use((config) => {
